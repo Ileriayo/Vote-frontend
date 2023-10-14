76ccaf3c93c55a9729d06f41f17131c24cf457be
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2023-10-14 04:58:07 +0100

    Fix spelling

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <div className="">
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='ps-5 pe-5 pt-3 pb-3'>
-                <Navbar.Brand href="#home">YALE DECIDES</Navbar.Brand>
+                <Navbar.Brand href="#home">YALI DECIDES</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
